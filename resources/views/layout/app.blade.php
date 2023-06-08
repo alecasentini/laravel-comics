7ec66362639a72a8e783c66faa3f56e65150e9fd
alecasentini <ale.casentini@gmail.com>
2023-06-08 15:57:05 +0200

    Creazione partials header e footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,6 +10,8 @@
 </head>
 
 <body>
+    @include( 'partials.header' )
+    @include ( 'partials.footer' )
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'Comics | DC';
+    return view('pages.home', compact('title'));
 });
